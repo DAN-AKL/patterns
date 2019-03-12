986f0922c7f835f577e4cbadea777a9d90f648d8
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-13 11:37:24 +1300

    Prettier-fied.

--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -1,36 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import Button from './Button.jsx';
-import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
+import Button from "./Button.jsx";
+import { storiesOf } from "@storybook/react";
+import {
+  withKnobs,
+  text,
+  boolean,
+  number,
+  select
+} from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 
-const stories = storiesOf('Buttons', module);
+const stories = storiesOf("Buttons", module);
 
 stories.addDecorator(withKnobs);
 
 const priorities = {
-  'Primary': 'primary',
-  'Secondary': 'secondary',
-  'Tertiary': 'tertiary'
-}
+  Primary: "primary",
+  Secondary: "secondary",
+  Tertiary: "tertiary"
+};
 
 const as = {
-  'Button': 'button',
-  'Anchor': 'anchor'
-}
+  Button: "button",
+  Anchor: "anchor"
+};
 
-stories
-  .add('Default', () => {
-    return (
-      <>
-        <Button
-          priority={select('Priority', priorities, 'primary')}
-          as={select('As', as, 'button')}
-          small={boolean('Small', false)}
-          disabled={boolean('Disabled', false)}
-          onClick={action('Button click')}
-        >I am a Button</Button>
-      </>
-    )
-  })
+stories.add("Default", () => {
+  return (
+    <>
+      <Button
+        priority={select("Priority", priorities, "primary")}
+        as={select("As", as, "button")}
+        small={boolean("Small", false)}
+        disabled={boolean("Disabled", false)}
+        onClick={action("Button click")}
+      >
+        I am a Button
+      </Button>
+    </>
+  );
+});
