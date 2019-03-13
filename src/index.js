70ea5bf87a00329f1f650067b1ce0894cf88f9d6
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-13 16:21:15 +1300

    Include LazyImage in exports.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export Button from "./components/Button";
 export Bento from "./components/Bento";
 export { BentoItem } from "./components/Bento";
 export { BentoHeader } from "./components/Bento";
+export LazyImage from "./components/LazyImage";
 export Poster from "./components/Poster";
 export Quote from "./components/Quote";
