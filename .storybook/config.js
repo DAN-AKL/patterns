a2823a9d187563c312ec42ff515996c1fdc65dfb
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-18 16:01:59 +1300

    stick the storybook component panel to the right.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { configure, addDecorator } from '@storybook/react';
-import Container from './Container';
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import Container from "./Container";
+
+addParameters({
+  options: {
+    panelPosition: "right"
+  }
+});
 
 addDecorator(story => <Container story={story} />);
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/components', true, /\.story\.js$/);
+const req = require.context("../src/components", true, /\.story\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
