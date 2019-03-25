17903ec8a08e4e1124265d76256686503ab9e57a
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-25 22:23:53 +1300

    A bunch of button snapshot tests.

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -2,6 +2,13 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Button from "./Button.jsx";
 
+import MockIcon from "./mock.svg";
+
+it("renders a button using the default props", () => {
+  const tree = renderer.create(<Button>I am a button</Button>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 it("renders a primary button", () => {
   const tree = renderer
     .create(
@@ -42,6 +49,64 @@ it("renders a seconadry anchor", () => {
         I am a button
       </Button>
     )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a tertiary button", () => {
+  const tree = renderer
+    .create(
+      <Button priority="tertiary" as="button">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a tertiary anchor", () => {
+  const tree = renderer
+    .create(
+      <Button priority="tertiary" as="anchor">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a button with an icon before the text", () => {
+  const tree = renderer
+    .create(
+      <Button icon={<MockIcon />} icon_placement="before">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a button with an icon after the text", () => {
+  const tree = renderer
+    .create(
+      <Button icon={<MockIcon />} icon_placement="after">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a disabled button", () => {
+  const tree = renderer
+    .create(<Button disabled={true}>I am a button</Button>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a small button", () => {
+  const tree = renderer
+    .create(<Button small={true}>I am a button</Button>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
