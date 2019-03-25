8208a5d426cdcac6b7fc0d041149f4f3ece69e86
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-25 15:15:11 +1300

    Export Breadcrumb and PageHeader correctly.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./sass/core.scss";
 
-export Button from "./components/Button";
 export Bento from "./components/Bento";
-export { BentoItem } from "./components/Bento";
 export { BentoHeader } from "./components/Bento";
+export { BentoItem } from "./components/Bento";
+export Breadcrumb from "./components/Breadcrumb";
+export Button from "./components/Button";
 export LazyImage from "./components/LazyImage";
+export PageHeader from "./components/PageHeader";
 export Poster from "./components/Poster";
 export Quote from "./components/Quote";
