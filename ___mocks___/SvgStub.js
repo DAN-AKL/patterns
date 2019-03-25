cfb8bc03aad13da3055fecc06c10f476866e1a04
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-25 22:22:19 +1300

    add an SVG mock file.

---
+++ ___mocks___/SvgStub.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+module.exports = function Arrow(props) {
+  return (
+    <svg viewBox="0 0 24 24">
+      <path d="M15.8198039,12 L21.2037682,12 L12,22.5185923" />
+    </svg>
+  );
+};
