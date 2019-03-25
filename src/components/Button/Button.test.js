7a355404aa9782f934cbe375caf1ea070659da5f
Cato <cato.johnston@digitalartsnetwork.com>
2019-03-25 21:37:03 +1300

    Test Primary / Secondary and <button> / <a> Buttons.

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -2,10 +2,43 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Button from "./Button.jsx";
 
-it("renders correctly", () => {
+it("renders a primary button", () => {
   const tree = renderer
     .create(
       <Button priority="primary" as="button">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a primary anchor", () => {
+  const tree = renderer
+    .create(
+      <Button priority="primary" as="anchor">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a seconadry button", () => {
+  const tree = renderer
+    .create(
+      <Button priority="secondary" as="button">
+        I am a button
+      </Button>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders a seconadry anchor", () => {
+  const tree = renderer
+    .create(
+      <Button priority="secondary" as="anchor">
         I am a button
       </Button>
     )
